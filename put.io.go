b1b1dfe4ad9e6f5edbf8915638aa5b026a896bbe
Robert Picard <mail@robert.io>
2015-04-07 15:46:50 -0700

    progress

--- put.io.go
+++ put.io.go
@@ -1,70 +1,69 @@
 package main
 
 import (
-    "net/http"
-    "fmt"
-    "log"
-    "io/ioutil"
-    "encoding/json"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+    "os"
 )
 
 type FileList struct {
-    Files   []*File `json:"files"`
-    Parent  File    `json:"parent"`
-    Status  string  `json:"status"`
+	Files  []*File `json:"files"`
+	Parent File    `json:"parent"`
+	Status string  `json:"status"`
 }
 
 type File struct {
-    ContentType string  `json:"content_type"`
-    CRC32   string  `json:"crc32"`
-    CreatedAt   string  `json:"created_at"`
-    FirstAccessedAt string  `json:"first_accessed_at"`
-    Icon    string  `json:"icon"`
-    Id  int `json:"id"`
-    IsMp4Available  bool    `json:"is_mp4_available"`
-    IsShared    bool    `json:"is_shared"`
-    Name    string  `json:"name"`
-    OpensubtitlesHash   string  `json:"opensubtitles_hash"`
-    ParentId    int `json:"parent_id"`
-    Screenshot  string  `json:"screenshot"`
-    Size    int `json:"size"`
+	ContentType       string `json:"content_type"`
+	CRC32             string `json:"crc32"`
+	CreatedAt         string `json:"created_at"`
+	FirstAccessedAt   string `json:"first_accessed_at"`
+	Icon              string `json:"icon"`
+	Id                int    `json:"id"`
+	IsMp4Available    bool   `json:"is_mp4_available"`
+	IsShared          bool   `json:"is_shared"`
+	Name              string `json:"name"`
+	OpensubtitlesHash string `json:"opensubtitles_hash"`
+	ParentId          int    `json:"parent_id"`
+	Screenshot        string `json:"screenshot"`
+	Size              int    `json:"size"`
 }
 
 type Client struct {
-    Token   string
+	Token string
 }
 
 func (c *Client) Get(path string) (resp *http.Response, err error) {
-    return http.Get(fmt.Sprint("https://api.put.io/v2/", path, "?oauth_token=", c.Token))
+	return http.Get(fmt.Sprint("https://api.put.io/v2/", path, "?oauth_token=", c.Token))
 }
 
-func (c *Client) ListFiles() {
-    resp, err := c.Get("files/list")
+func (c *Client) ListFiles() FileList{
+	resp, err := c.Get("files/list")
 
-    if err != nil {
-        log.Fatal(err)
-        return
-    }
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-    body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 
-    if err != nil {
-        log.Fatal(err)
-        return
-    }
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 
-    var f interface{}
+	var fl FileList
+	err = json.Unmarshal(body, &fl)
 
-    err = json.Unmarshal(body, &f)
-
-    fmt.Print(f)
-
+    return fl
 }
 
 func main() {
 
-    c := new(Client)
-    c.Token = ""
+	c := new(Client)
+	c.Token = os.Args[1]
 
-    c.ListFiles()
+	c.ListFiles()
 }
